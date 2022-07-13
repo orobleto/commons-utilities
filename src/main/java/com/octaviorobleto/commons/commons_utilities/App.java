0ceaf6e4f6d8fa17623e2b74aeaea36392b8393d
Octavio Robleto <octavio.robleto@gmail.com>
2022-07-13 15:03:44 -0300

    Inicio del Proyecto

---
+++ src/main/java/com/octaviorobleto/commons/commons_utilities/App.java
@@ -0,0 +1,13 @@
+package com.octaviorobleto.commons.commons_utilities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
